d9a9d2fa226db23f832dcb6f1640ec8908a13ccd
OEM Configuration (temporary user) <shubhamsahutheperfect@gmail.com>
2022-09-27 10:48:50 +0530

    final version of the array question

--- test.c
+++ test.c
@@ -1,4 +1,35 @@
 #include <stdio.h>
 int main(){
-    printf("hello world \n");
+    
+    int a, j, b, n;
+    
+    printf("Enter number of elements in the array: ");
+    scanf("%d", &b);
+    printf("Enter the elements: \n");
+
+    int arr[b];
+    int length = sizeof(arr)/sizeof(arr[0]);
+
+
+    for(int i = 0; i < b; ++i) {
+        scanf("%d", &arr[i]);
+    }
+    
+    printf("Displaying integers: \n");
+    
+    for(int i = 0; i < 5; ++i) {
+        printf("%d\n", arr[i]);
+    }
+
+
+    printf("The Duplicated elements are: ");
+    for(int i = 0; i < length; i ++){
+        for(j = i+1; j<length; j++){
+            if(arr[i] == arr[j]){
+                printf("%d", arr[j]);
+                printf(", ");
+            }
+        }
+    }
+return 0;
 }
